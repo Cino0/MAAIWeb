d9148dc4d0005f9c7371a6ea2053971d1328c15d
Matteo Mancinelli <matteo.mancinelli222@gmail.com>
2023-10-24 15:51:59 +0200

    commit

--- src/app/componenti/uploadat/uploadat.component.ts
+++ src/app/componenti/uploadat/uploadat.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, NgModule, OnInit } from '@angular/core';
+import { Anni } from 'src/app/interface/anni';
 
 @Component({
   selector: 'app-uploadat',
@@ -7,11 +8,14 @@ import { Component, NgModule, OnInit } from '@angular/core';
   styleUrls: ['./uploadat.component.css'],
 })
 export class UploadatComponent implements OnInit {
+
   visualizza: string = 'ATT';
   private nome: string = '';
   private data = new FormData();
   private dataIscr = new FormData();
   constructor(private http: HttpClient) {}
+  private anno=0;
+  anni : Anni[] =[]
 
   onChangeFile(event: any) {
     if (event.target.files.length > 0) {
@@ -53,5 +57,28 @@ export class UploadatComponent implements OnInit {
     console.log(this.visualizza);
   }
 
-  ngOnInit(): void {}
+
+  setAnni(){
+    let date = new Date();
+    this.anno=(date.getFullYear()*2)+1;
+    for(let i=this.anno-4;i<=this.anno; i++){
+      let app = (i-1)/2;
+      let  inizio :string = app.toString().substring(2,4);
+      let fin = app+1;
+      let fine : string = fin.toString().substring(2,4);
+      let annoVis= inizio + "/"+ fine;
+      let a :Anni = {value :i, viewValue:annoVis}
+      this.anni.push(a)
+      i++;
+    }
+  }
+
+  cambioAnno(e: any) {
+    this.anno=e;
+    console.log(this.anno)
+  }
+
+  ngOnInit(): void {
+    this.setAnni()
+  }
 }
